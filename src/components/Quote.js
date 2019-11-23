472c358e3194e01388a6f2d810688dca8c14a583
karaevelize <k.evelize@gmail.com>
2019-11-23 11:42:05 +0100

    Set Up

---
+++ src/components/Quote.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+class Quote extends Component {}
+
+export default Quote;
---
+++ src/components/QuoteSearcher.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class QuoteSearcher extends Component {
+  render() {
+    return (
+      <div className="quotes">
+        <h1>Quotes</h1>
+      </div>
+    );
+  }
+
+  state = {
+    quotes: [
+      {
+        _id: "5d91b45d9980192a317c8acc",
+        quoteText:
+          "Notice that the stiffest tree is most easily cracked, while the bamboo or willow survives by bending with the wind.",
+        quoteAuthor: "Bruce Lee"
+      },
+      {
+        _id: "5d91b45d9980192a317c8abe",
+        quoteText:
+          "Give me six hours to chop down a tree and I will spend the first four sharpening the axe.",
+        quoteAuthor: "Abraham Lincoln"
+      },
+      {
+        _id: "5d91b45d9980192a317c8955",
+        quoteText:
+          "Good timber does not grow with ease; the stronger the wind, the stronger the trees.",
+        quoteAuthor: "J. Willard Marriott"
+      }
+    ]
+  };
+}
+
+export default QuoteSearcher;
